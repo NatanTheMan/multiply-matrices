5cb06010add45723e4101816dbbb5bc6c59f2ae8
Natan <natanscosta7@gmail.com>
2025-12-04 22:22:08 -0300

    validate a single string and too a list of strings

--- lib/multiply_matrices.ex
+++ lib/multiply_matrices.ex
@@ -1,18 +1,57 @@
 defmodule MultiplyMatrices do
-  @moduledoc """
-  Documentation for `MultiplyMatrices`.
-  """
+  def main(_args) do
+    header()
+    description()
+    read_matrix_a()
+  end
 
-  @doc """
-  Hello world.
+  def header do
+    IO.puts("----Multiplicando Matrizes----")
+  end
 
-  ## Examples
+  def description do
+    IO.puts("Digite as Matrizes enserindo o tamanho de suas colunas e linhas")
+    IO.puts("E apos, ensira os numeros de uma linha por vez")
+  end
 
-      iex> MultiplyMatrices.hello()
-      :world
+  def read_matrix_a() do
+    IO.puts("Matriz A:")
+    read_dimensions_num("Colunas")
+    rows = read_dimensions_num("Linhas")
 
-  """
-  def hello do
-    :world
+    read_row(rows)
+    |> IO.inspect()
+  end
+
+  def read_dimensions_num(dimension) do
+    IO.gets("Digite o numero de #{dimension}: ")
+    |> validate()
+  end
+
+  def validate(input) when is_binary(input) do
+    case Integer.parse(input) do
+      {output, _} ->
+        output
+
+      :error ->
+        IO.puts("Input invalido, use apenas numeros")
+        System.halt(0)
+    end
+  end
+
+  def validate(input) when is_list(input) do
+    Enum.map(input, fn x -> validate(x) end)
+  end
+
+  def read_row(0), do: nil
+
+  def read_row(count) do
+    row =
+      IO.gets("-> ")
+      |> String.replace("\n", "")
+      |> String.split()
+      |> validate()
+
+    [row | read_row(count - 1)]
   end
 end
--- test/multiply_matrices_test.exs
+++ test/multiply_matrices_test.exs
@@ -2,7 +2,8 @@ defmodule MultiplyMatricesTest do
   use ExUnit.Case
   doctest MultiplyMatrices
 
-  test "greets the world" do
-    assert MultiplyMatrices.hello() == :world
+  test "return the list converted if all items are numbers" do
+    list = ["1", "2", "3"]
+    assert MultiplyMatrices.validate(list) == [1, 2, 3]
   end
 end
