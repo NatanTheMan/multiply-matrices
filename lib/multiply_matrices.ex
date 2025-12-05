c986772c199cda30c362ac4f77f722f7d37c3a78
Natan <natanscosta7@gmail.com>
2025-12-04 21:35:50 -0300

    init

---
+++ lib/multiply_matrices.ex
@@ -0,0 +1,18 @@
+defmodule MultiplyMatrices do
+  @moduledoc """
+  Documentation for `MultiplyMatrices`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MultiplyMatrices.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/multiply_matrices_test.exs
@@ -0,0 +1,8 @@
+defmodule MultiplyMatricesTest do
+  use ExUnit.Case
+  doctest MultiplyMatrices
+
+  test "greets the world" do
+    assert MultiplyMatrices.hello() == :world
+  end
+end
