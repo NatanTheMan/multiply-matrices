4985caee8c2b6a742d413441d2a4bf827b5678a6
Natan Costa <natanscosta7@gmail.com>
2026-01-07 08:26:07 -0300

    catch errors

--- src/index.php
+++ src/index.php
@@ -6,17 +6,21 @@ main();
 
 function main(): void
 {
-    echo "Type two matrices(A & B) to multiply them", PHP_EOL;
-    printMatrix(multiplyMatrices(readMatrix(), readMatrix()));
+    try {
+        echo "Type two matrices(A & B) to multiply them", PHP_EOL;
+        printMatrix(multiplyMatrices(readMatrix(), readMatrix()));
+    } catch (Error $e) {
+        echo $e->getMessage(), PHP_EOL;
+    }
 }
 
 function readMatrix(): Matrix
 {
     echo "Type matrix A:", PHP_EOL;
     echo "Type the number of columns: ";
-    $column = readline();
+    $column = validate(readline());
     echo "Type the number of rows: ";
-    $row = readline();
+    $row =  validate(readline());
     $grid = [];
     for ($i = 1; $i <= $row; $i++) {
         echo "Type the $i th line", PHP_EOL;
@@ -34,4 +38,13 @@ function printMatrix(Matrix $matrix): void
 function multiplyMatrices(Matrix $matrixA, Matrix $matrixB): Matrix
 {
     return $matrixA->multiply($matrixB);
+}
+
+function validate(string $input): int
+{
+    if (is_numeric($input)) {
+        return intval($input);
+    } else {
+        throw new Error("Invalid input: type just numbers");
+    }
 }
